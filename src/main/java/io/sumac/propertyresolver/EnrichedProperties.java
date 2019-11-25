c4e428e771feff18d44e099c824fa2f6da428034
Ross Stockman <ross.stockman@gmail.com>
2019-11-25 15:02:27 -0600

    refactoring

--- src/main/java/io/sumac/propertyresolver/EnrichedProperties.java
+++ src/main/java/io/sumac/propertyresolver/EnrichedProperties.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.function.Function;
@@ -16,6 +17,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.javaprop.JavaPropsMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
+import io.sumac.annotations.documentation.NonNull;
+
 /**
  * Extension of {@code java.util.Properties} that adds some additional
  * convenience methods.
@@ -23,23 +26,40 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
  * @author ross
  *
  */
-public class EnhancedProperties extends Properties {
+public class EnrichedProperties extends Properties {
 
 	private static final long serialVersionUID = -8938167962916354262L;
 
 	private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
 	private static final YAMLMapper YAML_MAPPER = new YAMLMapper();
 	private static final JavaPropsMapper PROPS_MAPPER = new JavaPropsMapper();
-	private Function<String, String> decryptor = (s) -> s;
 
-	public EnhancedProperties() {
+	private Function<String, String> decryptor = (s) -> s; // placeholder
+
+	/**
+	 * Create an empty property list
+	 */
+	public EnrichedProperties() {
 		super();
 	}
 
-	public EnhancedProperties(Properties properties) {
+	/**
+	 * Create a property list with the provided defaults
+	 * 
+	 * @param properties default properties
+	 */
+	public EnrichedProperties(Properties properties) {
 		super(properties);
 	}
 
+	/**
+	 * Same as {@link java.util.Properties#getProperty(String)} except the result is
+	 * optionally returned as a {@code String}
+	 * 
+	 * @param key the property key
+	 * @return Optional of String if found or Optional.empty() if not found
+	 */
+	@NonNull
 	public Optional<String> getString(String key) {
 		if (this.containsKey(key)) {
 			return Optional.of(decryptor.apply(this.getProperty(key)));
@@ -48,60 +68,116 @@ public class EnhancedProperties extends Properties {
 		}
 	}
 
+	/**
+	 * Same as {@link java.util.Properties#getProperty(String)} except the result is
+	 * optionally returned as a {@code Boolean}
+	 * 
+	 * @param key the property key
+	 * @return Optional of Boolean if found or Optional.empty() if not found
+	 */
+	@NonNull
 	public Optional<Boolean> getBoolean(String key) {
 		Optional<String> opt = getString(key);
 		if (!opt.isPresent()) {
 			return Optional.empty();
 		} else {
-			return Optional.of(PropertyResolverUtils.toBoolean(opt.get()));
+			return Optional.of(toBoolean(opt.get()));
 		}
 	}
 
+	/**
+	 * Same as {@link java.util.Properties#getProperty(String)} except the result is
+	 * optionally returned as an {@code Integer}
+	 * 
+	 * @param key the property key
+	 * @return Optional of Integer if found or Optional.empty() if not found
+	 */
+	@NonNull
 	public Optional<Integer> getInt(String key) {
 		Optional<String> opt = getString(key);
 		if (!opt.isPresent()) {
 			return Optional.empty();
 		} else {
-			return Optional.of(PropertyResolverUtils.toInt(opt.get()));
+			return Optional.of(toInt(opt.get()));
 		}
 	}
 
+	/**
+	 * Same as {@link java.util.Properties#getProperty(String)} except the result is
+	 * optionally returned as a {@code Long}
+	 * 
+	 * @param key the property key
+	 * @return Optional of Long if found or Optional.empty() if not found
+	 */
+	@NonNull
 	public Optional<Long> getLong(String key) {
 		Optional<String> opt = getString(key);
 		if (!opt.isPresent()) {
 			return Optional.empty();
 		} else {
-			return Optional.of(PropertyResolverUtils.toLong(opt.get()));
+			return Optional.of(toLong(opt.get()));
 		}
 	}
 
+	/**
+	 * Same as {@link java.util.Properties#getProperty(String)} except the result is
+	 * optionally returned as a {@code Double}
+	 * 
+	 * @param key the property key
+	 * @return Optional of Double if found or Optional.empty() if not found
+	 */
+	@NonNull
 	public Optional<Double> getDouble(String key) {
 		Optional<String> opt = getString(key);
 		if (!opt.isPresent()) {
 			return Optional.empty();
 		} else {
-			return Optional.of(PropertyResolverUtils.toDouble(opt.get()));
+			return Optional.of(toDouble(opt.get()));
 		}
 	}
 
+	/**
+	 * Same as {@link java.util.Properties#getProperty(String)} except the result is
+	 * optionally returned as a {@code Float}
+	 * 
+	 * @param key the property key
+	 * @return Optional of Float if found or Optional.empty() if not found
+	 */
+	@NonNull
 	public Optional<Float> getFloat(String key) {
 		Optional<String> opt = getString(key);
 		if (!opt.isPresent()) {
 			return Optional.empty();
 		} else {
-			return Optional.of(PropertyResolverUtils.toFloat(opt.get()));
+			return Optional.of(toFloat(opt.get()));
 		}
 	}
 
+	/**
+	 * This is a specialized toString method that returns a string representation of
+	 * this object formatted as a yaml file
+	 * 
+	 * @return a yaml formatted string representation of this object
+	 * @throws JsonProcessingException
+	 * @throws IOException
+	 */
+	@NonNull
 	public String toYamlString() throws JsonProcessingException, IOException {
 		return YAML_MAPPER.writeValueAsString(toJsonNode());
 	}
 
+	@NonNull
 	public String toJsonString() throws JsonProcessingException, IOException {
 		return toJsonString(false);
 	}
 
-	public String toJsonString(boolean pretty) throws JsonProcessingException, IOException {
+	/**
+	 * @param pretty
+	 * @return
+	 * @throws JsonProcessingException
+	 * @throws IOException
+	 */
+	public @NonNull String toJsonString(boolean pretty) throws JsonProcessingException, IOException {
 		if (pretty) {
 			return JSON_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(toJsonNode());
 		} else {
@@ -109,6 +185,7 @@ public class EnhancedProperties extends Properties {
 		}
 	}
 
+	@NonNull
 	public String toPropertiesString() throws JsonProcessingException, IOException {
 		boolean first = true;
 		StringBuilder builder = new StringBuilder();
@@ -157,13 +234,14 @@ public class EnhancedProperties extends Properties {
 		this.putAll(System.getenv());
 	}
 
-	public void setDecryptor(Function<String, String> decryptor) {
+	public void setDecryptor(@NonNull Function<String, String> decryptor) {
 		this.decryptor = decryptor;
 	}
 
-	public EnhancedProperties filterByRegex(String regex) {
+	@NonNull
+	public EnrichedProperties filterByRegex(String regex) {
 		Pattern pattern = Pattern.compile(regex);
-		EnhancedProperties result = new EnhancedProperties();
+		EnrichedProperties result = new EnrichedProperties();
 		this.forEach((k, v) -> {
 			Matcher matcher = pattern.matcher(k.toString());
 			if (matcher.matches()) {
@@ -173,14 +251,46 @@ public class EnhancedProperties extends Properties {
 		return result;
 	}
 
-	public EnhancedProperties getChildProperties(String parentPropertyKey) {
-		EnhancedProperties result = new EnhancedProperties();
+	@NonNull
+	public EnrichedProperties getChildProperties(String parentPropertyKey) {
+		EnrichedProperties result = new EnrichedProperties();
 		this.forEach((k, v) -> {
 			if (k instanceof String && k.toString().startsWith(parentPropertyKey)) {
 				result.put(k.toString().replace(parentPropertyKey + ".", ""), v);
 			}
 		});
 		return result;
+	}
+
+	private static int toInt(String value) {
+		rejectIfNull(value);
+		return Integer.valueOf(value);
+	}
+
+	private static long toLong(String value) {
+		rejectIfNull(value);
+		return Long.valueOf(value);
+	}
+
+	private static double toDouble(String value) {
+		rejectIfNull(value);
+		return Double.valueOf(value);
+	}
+
+	private static float toFloat(String value) {
+		rejectIfNull(value);
+		return Float.valueOf(value);
+	}
+
+	private static boolean toBoolean(String value) {
+		rejectIfNull(value);
+		return Boolean.valueOf(value);
+	}
+
+	private static void rejectIfNull(Object value) {
+		if (Objects.isNull(value)) {
+			throw new NullPointerException();
+		}
 	}
 
 }
--- src/main/java/io/sumac/propertyresolver/PropertyResolverException.java
+++ src/main/java/io/sumac/propertyresolver/PropertyResolverException.java
@@ -1,64 +1,35 @@
 package io.sumac.propertyresolver;
 
 import java.io.IOException;
-import java.sql.SQLException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 public abstract class PropertyResolverException extends RuntimeException {
 
 	private static final long serialVersionUID = 5679265155061006371L;
 
-	public PropertyResolverException(String msg) {
+	protected PropertyResolverException(String msg) {
 		super(msg);
 	}
 
-	public PropertyResolverException(String msg, Throwable t) {
+	protected PropertyResolverException(String msg, Throwable t) {
 		super(msg, t);
 	}
 
-	public static PropertyResolverException errorReadingClasspathPropertyFile(String propertiesFile, IOException e) {
-		return new PropertySourceNotFoundException("File not found on classpath: '" + propertiesFile + "'", e);
+	public static PropertyResolverException from(IOException e) {
+		return new UnexpectedIOException("An unexpected " + e.getClass().getName() + " occurred: " + e.getMessage(), e);
 	}
 
-	public static PropertyResolverException errorReadingClasspathPropertyFile(String propertiesFile,
-			PropertyResolverException e) {
-		return new PropertySourceNotFoundException("File not found on classpath: '" + propertiesFile + "'", e);
+	public static PropertyResolverException from(JsonProcessingException e) {
+		return new UnexpectedIOException(
+				"An unexpected parsing/IO " + e.getClass().getName() + " occurred: " + e.getMessage(), e);
 	}
 
-	public static PropertyResolverException errorReadingPropertyFile(String propertiesFile, IOException e) {
-		return new PropertySourceNotFoundException("File not read: '" + propertiesFile + "'", e);
-	}
-
-	public static PropertyResolverException errorReadingPropertyFile(String propertiesFile,
-			PropertyResolverException e) {
-		return new PropertySourceNotFoundException("File not read: '" + propertiesFile + "'", e);
-	}
-
-	public static PropertyResolverException errorReadingInputStream(IOException e) {
-		return new PropertySourceNotFoundException("InputStream error", e);
-	}
-
-	public static PropertyResolverException sqlError(SQLException e) {
-		return new SqlErrorException("SQL error", e);
-	}
-
-	static class PropertySourceNotFoundException extends PropertyResolverException {
+	static class UnexpectedIOException extends PropertyResolverException {
 
 		private static final long serialVersionUID = -6823604987227128796L;
 
-		private PropertySourceNotFoundException(String msg) {
-			super(msg);
-		}
-
-		private PropertySourceNotFoundException(String msg, Throwable t) {
-			super(msg, t);
-		}
-	}
-
-	static class SqlErrorException extends PropertyResolverException {
-
-		private static final long serialVersionUID = 6566866076070089166L;
-
-		private SqlErrorException(String msg, Throwable t) {
+		private UnexpectedIOException(String msg, Throwable t) {
 			super(msg, t);
 		}
 	}
